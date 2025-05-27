194e04fc1519124dde77e5b5b76aef0147a2b48a
Morgan Ogola <morganogola@gmail.com>
2025-05-27 12:45:53 +0300

    added navbar styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
   return (
-    <div className='navbar'></div>
-  )
-}
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo image here" />
+        <p>SHOPPER</p>
+      </div>
 
-export default Navbar
+      <ul className="nav-menu">
+        <li>Shop<hr/></li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart icon here" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
