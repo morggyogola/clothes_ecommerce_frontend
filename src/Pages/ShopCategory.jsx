75258bc09adc0728078fd2a5863fa6f21c847205
Morgan Ogola <morganogola@gmail.com>
2025-05-28 17:16:39 +0300

    Added the login and signup page

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -35,9 +35,7 @@ const ShopCategory = (props) => {
           }
         })}
       </div>
-      <div className="shopcategory-loadmore">
-        Explore More
-      </div>
+      <div className="shopcategory-loadmore">Explore More</div>
     </div>
   );
 };
